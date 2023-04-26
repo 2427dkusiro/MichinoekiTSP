8fc71f47dca7a94ec752a3d64a23e747fec1add2
2427dkusiro <kantake1969@gmail.com>
2023-04-26 12:02:29 +0900

    ビジュアライザ実装

--- MichinoekiTSPCLI/Program.cs
+++ MichinoekiTSPCLI/Program.cs
@@ -8,11 +8,8 @@ class Program
     {
         MichinoekiResourceManager manager = MichinoekiResourceManager.CreateInstance();
 
-        var sta1 = manager.Michinoekis.First(x => x.Name == "三笠");
-        var sta2 = manager.Michinoekis.First(x => x.Name == "スタープラザ 芦別");
+        
 
         // FetchRoute(rawSaveDir, sta1, sta2);
-        var routes = manager.Routes;
-        var str = $"""[{string.Join(',', routes[0].PolylineDecoded.Select(x => $"[{x.Latitude},{x.Longitude}]"))}]""";
     }
 }
--- MichinoekiVisualizerWindows/MainWindow.xaml.cs
+++ MichinoekiVisualizerWindows/MainWindow.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Text;
+﻿using Microsoft.Web.WebView2.Wpf;
+
+using Path = System.IO.Path;
+
+using System.Reflection;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MichinoekiTSP.Data;
 
 namespace MichinoekiVisualizerWindows;
 /// <summary>
@@ -18,5 +24,67 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private WebView2 webView;
+
+    private async void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+        var execDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        var html = Path.Combine(execDir, "Index.html");
+
+        webView = new WebView2()
+        {
+            Source = new Uri($"file:///{html}")
+        };
+        MainGrid.Children.Add(webView);
+
+        webView.ContentLoading += async (sender, e) =>
+        {
+            await InitializeMap();
+        };
+    }
+
+    private async Task SetView(double lat, double lng)
+    {
+        var result = await webView.CoreWebView2.ExecuteScriptAsync($"""setView({lat},{lng})""");
+    }
+
+    private async Task SetZoom(int level)
+    {
+        var result = await webView.CoreWebView2.ExecuteScriptAsync($"""setZoom({level})""");
+    }
+
+    private async Task AddMarker(double lat, double lng, string name)
+    {
+        var result = await webView.CoreWebView2.ExecuteScriptAsync($"""addMarker({lat},{lng},"{name}")""");
+    }
+
+    private async Task AddPolyline(IEnumerable<GeometryPoint> points, string color)
+    {
+        var arrayString = $"""[{string.Join(',', points.Select(x => $"[{x.Latitude},{x.Longitude}]"))}]""";
+        var result = await webView.CoreWebView2.ExecuteScriptAsync($"""addPolyline({arrayString},"{color}")""");
+    }
+
+    private async Task InitializeMap()
+    {
+        // TODO:もうちょっとちゃんと待つ
+        await Task.Delay(100);
+        await SetView(43.06878725573487, 141.35072226585388);
+        await Task.Delay(100);
+        await SetZoom(10);
+
+        var manager = MichinoekiResourceManager.CreateInstance();
+        foreach (var point in manager.Michinoekis)
+        {
+            await AddMarker(point.Latitude, point.Longitude, point.Name);
+        }
+
+        await Task.Delay(100);
+
+        foreach (var route in manager.Routes)
+        {
+            await AddPolyline(route.PolylineDecoded, "#0000bb");
+        }
     }
 }
